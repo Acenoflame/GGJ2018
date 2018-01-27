323f050b06ea0200d5ba9617f103ec371f0369f4
DESKTOP-66KHEKR\kings <kingsnake991@hotmail.it>
2018-01-27 16:53:20 +0100

    InputManager controllers Movement players Movement pointer

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public int speed = 3;
+
+    public Transform _player;
+    public Transform _target;
+
+    public string strXLeftStick;
+    public string strYLeftStick;
+    public string strXRightStick;
+    public string strYRightStick;
+
+    float movXLeftStick;
+    float movYLeftStick;
+    float movXRightStick;
+    float movYRightStick;
+
+
+    // Use this for initialization
+    void Start () {
+        movXLeftStick = Input.GetAxis(strXLeftStick);
+        movYLeftStick = Input.GetAxis(strYLeftStick);
+        movXRightStick = Input.GetAxis(strXRightStick);
+        movYRightStick = Input.GetAxis(strYRightStick);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        movXLeftStick = Input.GetAxis(strXLeftStick);
+        movYLeftStick = Input.GetAxis(strYLeftStick);
+        movXRightStick = Input.GetAxis(strXRightStick);
+        movYRightStick = Input.GetAxis(strYRightStick);
+
+        _player.Translate (new Vector3(movXLeftStick, movYLeftStick) *Time.deltaTime*speed);
+        _target.Translate (new Vector3(movXRightStick, movYRightStick) *Time.deltaTime*speed);
+	}
+}
