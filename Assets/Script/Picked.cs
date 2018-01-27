7da082385ac500c15abf01fc0b3c976bca08d3a2
fdc9 <fra_sdc@hotmail.it>
2018-01-27 17:02:44 +0100

    fix merge

---
+++ Assets/Script/Picked.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Picked : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+
+
+		if (coll.gameObject.GetComponent<Movement> ()) {
+			coll.gameObject.GetComponent<Fire> ().munitions++;
+			Destroy (this.gameObject);
+		}
+	}
+}
